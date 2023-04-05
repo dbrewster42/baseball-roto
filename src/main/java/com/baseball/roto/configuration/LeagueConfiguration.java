404691d395304a4c9a4503f81dff93cdc40a2778
R Devon Brewster <devonbrewster42@gmail.com>
2023-04-05 12:02:19 -0500

    simplified configuration

--- src/main/java/com/baseball/roto/configuration/LeagueConfiguration.java
+++ src/main/java/com/baseball/roto/configuration/LeagueConfiguration.java
@@ -1,7 +1,13 @@
 package com.baseball.roto.configuration;
 
+import com.baseball.roto.mapper.StatsMapper;
 import com.baseball.roto.model.League;
+import com.baseball.roto.model.entity.Stats;
+import com.baseball.roto.repository.StatsRepository;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +15,21 @@ import static com.baseball.roto.validation.InputValidator.validatedLeague;
 
 @Configuration
 public class LeagueConfiguration {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
     public League league(@Value("${league}") String league) {
         return validatedLeague(league);
+    }
+
+    @Bean
+    public StatsRepository<? extends Stats> repository(League league) {
+        return applicationContext.getBean(league.getRepository());
+    }
+
+    @Bean
+    public StatsMapper<? extends Stats> statsMapper(League league) {
+        return Mappers.getMapper(league.getMapper());
     }
 }
--- src/main/java/com/baseball/roto/model/League.java
+++ src/main/java/com/baseball/roto/model/League.java
@@ -4,6 +4,10 @@ import com.baseball.roto.mapper.ChampStatsMapper;
 import com.baseball.roto.mapper.PsdStatsMapper;
 import com.baseball.roto.mapper.StandardStatsMapper;
 import com.baseball.roto.mapper.StatsMapper;
+import com.baseball.roto.repository.ChampRepository;
+import com.baseball.roto.repository.PsdRepository;
+import com.baseball.roto.repository.StandardRepository;
+import com.baseball.roto.repository.StatsRepository;
 import lombok.Getter;
 
 import java.util.Arrays;
@@ -12,22 +16,25 @@ import java.util.stream.Collectors;
 
 @Getter
 public enum League {
-    CHAMPIONS(14, 6, 4, 4, ChampStatsMapper.class),
-    PSD(10, 5, 4, 3, PsdStatsMapper.class),
-    STANDARD(12, 5, 4, 3, StandardStatsMapper.class);
+    CHAMPIONS(14, 6, 4, 4, ChampStatsMapper.class, ChampRepository.class),
+    PSD(10, 5, 4, 3, PsdStatsMapper.class, PsdRepository.class),
+    STANDARD(12, 5, 4, 3, StandardStatsMapper.class, StandardRepository.class);
 
     private final int numberOfTeams;
     private final int statColumns;
     private final int hitCountingStats;
     private final int pitchCountingStats;
     private final Class<? extends StatsMapper<?>> mapper;
+    private final Class<? extends StatsRepository<?>> repository;
 
-    League(int numberOfTeams, int statColumns, int hitCountingStats, int pitchCountingStats, Class<? extends StatsMapper<?>> mapper) {
+    League(int numberOfTeams, int statColumns, int hitCountingStats, int pitchCountingStats,
+           Class<? extends StatsMapper<?>> mapper, Class<? extends StatsRepository<?>> repository) {
         this.numberOfTeams = numberOfTeams;
         this.statColumns = statColumns;
         this.hitCountingStats = hitCountingStats;
         this.pitchCountingStats = pitchCountingStats;
         this.mapper = mapper;
+        this.repository = repository;
     }
 
     public static List<String> leagueNames() {
