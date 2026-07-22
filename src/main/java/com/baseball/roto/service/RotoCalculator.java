ba0bbf02d5128c084b02d50fff3d09bddf4f9340
Devon <devonbrewster42@gmail.com>
2026-07-22 11:44:26 -0500

    bugfix and upgrade

--- src/main/java/com/baseball/roto/service/RotoCalculator.java
+++ src/main/java/com/baseball/roto/service/RotoCalculator.java
@@ -63,7 +63,7 @@ public class RotoCalculator {
             float tieModifier = .5F;
             int tiedRank = tiedRanks.get(i);
             while (i < tiedRanks.size() - 1 && tiedRank == tiedRanks.get(i + 1)){
-                tieModifier += .5;
+                tieModifier += .5F;
                 i++;
             }
             for (List<Float> statList : stats.values()){
